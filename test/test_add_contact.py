6d03d2c6b8f5c12567734682405cb020e80b7d25
NataliGorod <natalygorod@redsoft.ru>
2018-11-17 16:08:27 +0300

    созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from newcontact import Newcontact
-from application import Application
+from model.newcontact import Newcontact
+from fixture.application import Application
 
 
 @pytest.fixture
