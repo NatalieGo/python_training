8bdff794f65b1b6c9a3785bc875c3fae860d7c9c
NataliGorod <natalygorod@redsoft.ru>
2018-12-02 20:56:29 +0300

    Добавлено выполнение предусловий

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,7 @@
+from model.newcontact import Newcontact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,16 +2,26 @@ from model.newcontact import Newcontact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.modify_first_contact(Newcontact(firstname="New Firstname"))
 
 def test_modify_contact_middlename(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.modify_first_contact(Newcontact(middlename="New Middlename"))
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.modify_first_contact(Newcontact(lastname="New Lastname"))
 
 def test_modify_contact_mobile(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.modify_first_contact(Newcontact(mobile="+79222222222"))
 
 def test_modify_contact_address(app):
+    if app.contact.count() == 0:
+        app.contact.create(Newcontact(firstname="Testfirstname"))
     app.contact.modify_first_contact(Newcontact(address="New address"))
